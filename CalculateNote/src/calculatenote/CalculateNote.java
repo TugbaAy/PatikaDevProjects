0c0e6115a2f12a115045e43c0ec9c165468b18ff
TugbaAy <tuggbaayy@gmail.com>
2022-04-29 12:44:44 +0300

    calculate note of lessons

--- CalculateNote/src/calculatenote/CalculateNote.java
+++ CalculateNote/src/calculatenote/CalculateNote.java
@@ -19,7 +19,7 @@ public class CalculateNote {
     public static void main(String[] args) {
         // TODO code application logic here
 
-        int math, physics, chemistry, turkish, history, music;
+        int math = 0, physics = 0, chemistry = 0, turkish = 0, history = 0, music = 0;
         Scanner input = new Scanner(System.in);
 
         System.out.println("Matematik notunuzu giriniz : ");
@@ -40,6 +40,15 @@ public class CalculateNote {
         System.out.println("Müzik notunuzu giriniz : ");
         physics = input.nextInt();
 
+        
+        int totalPointOfLessons = math + physics + chemistry + turkish + history + music;
+        double averageOfLessons = totalPointOfLessons / 6;
+        System.out.println("Ders notu ortalamanız : " +averageOfLessons);
+        
+        String result = averageOfLessons >= 60 ? "Geçtiniz" : "Kaldınız." ;
+        
+        System.out.println("Ortalamanızın sonucu olarak derslerden : " +result );
+        
     }
 
 }
